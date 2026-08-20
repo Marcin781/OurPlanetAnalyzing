fe375ae0fe9289639a931e79dae0bdf8a2b41f38
Maarrcin <wielebmarcin@gmail.com>
2026-08-20 17:59:32 +0200

    feat: add voivodeship capital city coordinates

---
+++ cities.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+# Voivodeship capitals / co-capitals used as representative urban points.
+# Kujawsko-Pomorskie and Lubuskie have two official voivodeship seats,
+# therefore the dataset contains 18 cities for 16 voivodeships.
+VOIVODESHIP_CAPITALS = {
+    "Białystok": {"voivodeship": "Podlaskie", "latitude": 53.1325, "longitude": 23.1688},
+    "Bydgoszcz": {"voivodeship": "Kujawsko-Pomorskie", "latitude": 53.1235, "longitude": 18.0084},
+    "Toruń": {"voivodeship": "Kujawsko-Pomorskie", "latitude": 53.0138, "longitude": 18.5984},
+    "Gdańsk": {"voivodeship": "Pomorskie", "latitude": 54.3520, "longitude": 18.6466},
+    "Gorzów Wielkopolski": {"voivodeship": "Lubuskie", "latitude": 52.7325, "longitude": 15.2369},
+    "Zielona Góra": {"voivodeship": "Lubuskie", "latitude": 51.9356, "longitude": 15.5062},
+    "Katowice": {"voivodeship": "Śląskie", "latitude": 50.2649, "longitude": 19.0238},
+    "Kielce": {"voivodeship": "Świętokrzyskie", "latitude": 50.8661, "longitude": 20.6286},
+    "Kraków": {"voivodeship": "Małopolskie", "latitude": 50.0647, "longitude": 19.9450},
+    "Lublin": {"voivodeship": "Lubelskie", "latitude": 51.2465, "longitude": 22.5684},
+    "Łódź": {"voivodeship": "Łódzkie", "latitude": 51.7592, "longitude": 19.4560},
+    "Olsztyn": {"voivodeship": "Warmińsko-Mazurskie", "latitude": 53.7784, "longitude": 20.4801},
+    "Opole": {"voivodeship": "Opolskie", "latitude": 50.6751, "longitude": 17.9213},
+    "Poznań": {"voivodeship": "Wielkopolskie", "latitude": 52.4064, "longitude": 16.9252},
+    "Rzeszów": {"voivodeship": "Podkarpackie", "latitude": 50.0413, "longitude": 21.9990},
+    "Szczecin": {"voivodeship": "Zachodniopomorskie", "latitude": 53.4285, "longitude": 14.5528},
+    "Warszawa": {"voivodeship": "Mazowieckie", "latitude": 52.2297, "longitude": 21.0122},
+    "Wrocław": {"voivodeship": "Dolnośląskie", "latitude": 51.1079, "longitude": 17.0385},
+}
